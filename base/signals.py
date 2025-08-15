d463de860d5574ba5559ae027257df64947cdf42
Gustavo Oliveira Benites <95330379+BenitesG@users.noreply.github.com>
2025-08-15 14:28:56 -0300

    Update signals.py

--- base/signals.py
+++ base/signals.py
@@ -8,7 +8,7 @@ def sendEmailLogin(sender, request, user, **kwargs):
     print(f"Enviando e-mail para: {user.email}")
     
     subject = "Login realizado com sucesso!"
-    message = f"Olá {user.username}, você acabou de ser registrado no desafio técnico da fidelity!"
+    message = f"Olá {user.username}, você acabou de ser registrado no desafio técnico fidelity!"
     from_email = settings.DEFAULT_FROM_EMAIL
     recipient_list = [user.email]
 
